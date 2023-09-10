a395d19e2d8b0bff0a9337343f5ed1434dccf7d7
thirumala reddy <thirumalareddy797@gmail.com>
2023-09-10 09:39:08 +0530

    while,jira confluence

---
+++ class7.py
@@ -0,0 +1,62 @@
+# while loop:
+    # whenever you need loop continus
+
+
+
+'''
+x = int(input("eneter the value:"))
+while x<8:
+    print(x)
+    x = x+1
+
+while x<8:#3<8,4<8,5<8
+    if x == 5:break 
+    print(x)#3#4
+    x = x+1#4#5
+
+while x<8:#3<8,4<8,5<8
+    if x == 5:
+        x = x +1
+        continue
+        
+        
+    print(x)#3#4
+    x = x+1#4#5
+while True:
+     x = int(input("enter the value:"))
+     print(x)
+'''
+x = 0
+while True:
+    if x == 20:
+        break
+    print(f" {x} hellow world")
+    x = x+1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
